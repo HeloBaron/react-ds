a5fb2dd895a2392f095a118e4de05332a836227c
HeloBaron <helo.athayde@gmail.com>
2023-01-12 15:55:11 -0300

    .

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,10 +15,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, {configType}) => {
+  viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/react-ds/'
     }
+
     return config
-  }
+  },
 }
