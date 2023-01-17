a68c529dec8ff8deba9b0e91a16d4a78f10cd13d
HeloBaron <helo.athayde@gmail.com>
2023-01-17 14:38:48 -0300

    update: Add axios and msw

--- src/pages/SingIn.stories.tsx
+++ src/pages/SingIn.stories.tsx
@@ -1,13 +1,25 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { within, userEvent, waitFor } from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
+import { rest } from 'msw'
 import { SignIn } from './Singin'
 
 export default {
     title: 'Pages/Sing in',
     component: SignIn,
     args: {},
-    argTypes: {}
+    argTypes: {},
+    parameters: {
+        msw: {
+            handlers: [
+                rest.post('/sessions', (req, res, ctx) => {
+                    return res(ctx.json({
+                        message: 'Login!'
+                    }))
+                })
+            ],
+        },
+    }
 } as Meta
 
 export const Default: StoryObj = {
